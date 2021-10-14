ae9083fcca89a3d3d52a83fb262442b672869dc6
VictorTao1998 <jit124@ucsd.edu>
2021-10-14 16:26:27 -0700

    bug

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ export PYTHONWARNINGS="ignore"
 
 python /cephfs/jianyu/ActiveStereoNet/train_activestereo.py \
 --config /cephfs/jianyu/ActiveStereoNet/disparity/configs/remote_train_gan.yaml \
---log_freq 100 --logdir /cephfs/jianyu/eval/activestereo_train1
+--log_freq 100 --logdir /cephfs/jianyu/eval/activestereo_train
